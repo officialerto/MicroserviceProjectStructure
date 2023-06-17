5b43bd4459ce1a9196127bff53b237e1cc8b7845
Ertugrul <57365047+officialerto@users.noreply.github.com>
2023-06-17 23:04:51 +0300

    .MicroserviceProject_LastUpdate_17.06.23

---
+++ SellingBuddy/src/BuildingBlocks/EventBus/EventBus.Base/Events/IntegrationEvents.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Base.Events
+{
+    public class IntegrationEvents
+    {
+
+        [JsonProperty]
+        public Guid Id { get; private set; }
+        [JsonProperty]
+        public DateTime CreatedDate { get; private set; }
+
+
+        [JsonConstructor]
+        public IntegrationEvents(Guid id, DateTime createdDate)
+        {
+            Id = id;
+            CreatedDate = createdDate;
+        }
+
+        public IntegrationEvents()
+        {
+            Id = Guid.NewGuid();
+            CreatedDate = DateTime.Now;
+        }
+
+    }
+}
